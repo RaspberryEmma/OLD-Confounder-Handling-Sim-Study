5caf901b0bc5174712dd24db72a5631b02d35efa
Emma <91637007+RaspberryEmma@users.noreply.github.com>
2024-12-10 16:01:16 +0000

    latest results, precomputed seeds

--- bash/update_and_launch.sh
+++ bash/update_and_launch.sh
@@ -30,7 +30,7 @@ module load languages/python/3.12.3
 
 # submit simulation to BP HPC
 #sbatch launch_BP.sh
-for ((i=1;i<3;i++));do
+for ((i=1;i<4;i++));do
 	for ((j=5;j<7;j++));do
 		python generate_BP_job_file.py $i $j
 		echo "Submitting job: launch_BP_run_"$i"_scenario_"$j".sh"
